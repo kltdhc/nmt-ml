1182f509591f15cbe1a43eca434dd0264beb5419
kltdhc <hcwanghao@gmail.com>
2018-03-29 17:10:16 +0800

    add output

--- src/data_loader.py
+++ src/data_loader.py
@@ -134,4 +134,6 @@ def main():
         sess.run(tf.global_variables_initializer())
         for i in range(100):
             model.train_all(sess, train_qsent, train_qlen, train_asent[0], train_alen[0])
+            print('train...')
             rt = model.test(sess, dev_qsent, dev_qlen)
+            print('test...')
