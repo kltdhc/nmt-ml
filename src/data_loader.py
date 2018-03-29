9f05dcab83349379ecc4446f50938fbd4ed278e5
kltdhc <hcwanghao@gmail.com>
2018-03-29 19:59:00 +0800

    bugfix

--- src/data_loader.py
+++ src/data_loader.py
@@ -159,4 +159,4 @@ def main():
             model.train_all(sess, train_qsent, train_qlen, train_asent[0], train_alen[0])
             print('train...')
             rt = model.test(sess, dev_qsent, dev_qlen)
-            print('test...bleu=', eval(test_asent, rt[0]))
+            print('test...bleu=', eval(test_asent[0], rt[0]))
